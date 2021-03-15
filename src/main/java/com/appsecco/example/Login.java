8860a820ce976bea5d0b09d257ed914a5556ecc4
Gem Immanuel <25984676+gemgit7@users.noreply.github.com>
2021-03-15 12:51:38 -0400

    Update Login.java

--- src/main/java/com/appsecco/example/Login.java
+++ src/main/java/com/appsecco/example/Login.java
@@ -1,7 +1,7 @@
 /*
  * $Id$
  *
- * Licensed to the Apache Software Foundation (ASF) under one
+ * Licensed to the Apache Software Foundation (ASdF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
